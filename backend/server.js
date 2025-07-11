d1daca566c5bdf82bebb252c97aeaa2c86100f1b
Hector Acosta <1Hector.AC@gmail.com>
2025-07-11 13:06:54 -0700

    Added web socket setup for backend.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const http = require('http');
+const {Server} = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {cors: {origin:'*', methods:['GET','POST']}});
 
 app.get('/', (req,res) => {
 	res.send('<h1>Test of Node App.</h1>')
 });
 
-app.listen(3000, () => {console.log("Listening on 3000...")});
+io.on('connection', socket => {
+	console.log(socket.id + " connected");
+
+
+	io.on('disconnect', () => {
+		console.log(socket.id + " disconnected");
+	})
+})
+
+server.listen(5000, () => {console.log("Listening on 5000...")});
